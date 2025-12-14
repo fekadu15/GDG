e79da35b9ef83397e08cef4f0dc008933712fc92
fekadu argeta <fekaduargeta@gmail.com>
2025-12-14 19:34:26 +0300

    week 1 task

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My-To-Do App</h1>
+        <p>Created with react app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
