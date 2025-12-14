d957ad2b8a318a9a86ca500117c6570c48d92910
fekadu argeta <fekaduargeta@gmail.com>
2025-12-14 23:13:02 +0300

    week 3 task props

--- week_3/src/components/MemberCard.jsx
+++ week_3/src/components/MemberCard.jsx
@@ -1,6 +1,7 @@
  import React from "react";
  const styles = {
   card: {
+     backgroundColor:"lightgray",
     width: "250px",
     padding: "15px",
     borderRadius: "10px",
@@ -17,13 +18,14 @@
   }
 };
 
-export default function MemberCard({ name, role, photo, bio }) {
+export default function MemberCard(props) {
   return (
     <div style={styles.card}>
-      <img src={photo} alt={name} style={styles.image} />
-      <h3>{name}</h3>
-      <p style={styles.role}>{role}</p>
-      <p>{bio}</p>
+      <img src={props.photo} alt={props.name} style={styles.image} />
+      <h3>{props.name}</h3>
+      <p style={styles.role}>{props.role}</p>
+      <p >{props.bio}</p>
     </div>
   );
 }
+
--- week_3/src/components/Team.jsx
+++ week_3/src/components/Team.jsx
@@ -6,35 +6,39 @@ export default function Team() {
       id: 1,
       name: "Fike",
       role: "Frontend Developer",
-      photo: "https://via.placeholder.com/150",
+      photo: "image2.jpg",
       bio: "Passionate about building clean UI."
     },
     {
       id: 2,
       name: "Halid",
       role: "Backend Developer",
-      photo: "https://via.placeholder.com/150",
+      photo: "image2.jpg",
       bio: " worke with APIs, databases"
     },
     {
       id: 3,
       name: "Fira",
       role: "UI/UX Designer",
-      photo: "https://via.placeholder.com/150",
+      photo: "image2.jpg",
       bio: "Focused on creating beautiful designs."
     }
   ];
 
   const styles = {
   title: {
+    color:"red",
+    
     textAlign: "center",
     marginBottom: "20px"
   },
   container: {
+    
     display: "flex",
     gap: "20px",
     justifyContent: "center",
-    flexWrap: "wrap"
+    flexWrap: "wrap", 
+    
   },
 
   
@@ -44,15 +48,17 @@ export default function Team() {
     <div>
       <h1 style={styles.title}>Our Team</h1>
       <div style={styles.container}>
-        {members.map(member => (
-          <MemberCard
-            id = {member.id}
-            name={member.name}
-            role={member.role}
-            photo={member.photo}
-            bio={member.bio}
-          />
-        ))}
+         {
+           members.map((member,id) => (
+            <MemberCard
+            id={member.id}
+            name= {member.name}
+            bio = {member.bio}
+            role = {member.role}
+            photo = {member.photo}
+            />
+            )) 
+          }
       </div>
     </div>
   );
